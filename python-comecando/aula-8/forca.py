5f1b1877348ec1db56b8544fd90bca687c35315c
jeferson-felix <jeferson.felix2002@outlook.com>
2023-05-29 16:51:58 -0300

    aula-8

---
+++ python-comecando/aula-8/forca.py
@@ -0,0 +1,10 @@
+def jogar():
+    print(f'{"-" * 40}')
+    print(f'{"Bem vindo ao jogo da forca":^40}')
+    print(f'{"-" * 40}')
+
+    print('Fim de jogo.')
+
+
+if __name__ == "__main__":
+    jogar()
