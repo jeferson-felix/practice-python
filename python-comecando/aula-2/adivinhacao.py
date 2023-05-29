31523a70e1c932dda8fe939332a7cb0eec842b82
jeferson-felix <jeferson.felix2002@outlook.com>
2023-05-29 11:31:36 -0300

    aula-2

--- python-comecando/aula-2/adivinhacao.py
+++ python-comecando/aula-2/adivinhacao.py
@@ -1,1 +1,14 @@
+print(f'{"-"*35}')
 print('Bem vindo ao jogo de adivinhação')
+print(f'{"-"*35}')
+
+numero_secreto = 40
+
+chute = int(input('Digite o seu número: '))
+
+print(f'Você digitou o número {chute}')
+
+if numero_secreto == chute:
+    print('Você acertou!')
+else:
+    print(f'Você errou!, o número secreto era {numero_secreto}')
