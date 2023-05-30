b59e501628bb9d5d5506d7621cbb8c8b2cb322c8
jeferson-felix <jeferson.felix2002@outlook.com>
2023-05-30 13:28:43 -0300

    aula-4

---
+++ python-avancando/aula-4/dictionary.py
@@ -0,0 +1,6 @@
+# utilizando dictionary
+# ao lado esquerdo possui a chave e ao lado direito o valor
+instrutores = {'Nico': 39, 'Flavio': 37, 'Marcos': 30}
+
+# consultando a idade pelo nome
+print(instrutores['Nico'])
---
+++ python-avancando/aula-4/listas-tuplas.py
@@ -0,0 +1,15 @@
+# criando as pessoas utilizando tuplas
+pessoa1 = ("Nico", 39)
+pessoa2 = ("Flavio", 37)
+pessoa3 = ("Marcos", 30)
+
+# colocando as tuplas em uma lista
+instrutores = [pessoa1, pessoa2, pessoa3]
+
+# mostrando a lista
+print(instrutores)
+
+print('')
+
+# consultando a idade
+print(instrutores[1][1])
