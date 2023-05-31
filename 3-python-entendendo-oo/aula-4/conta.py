a17f408ca8f1f7cf230f31537150ff565e1bdf22
jeferson-felix <jeferson.felix2002@outlook.com>
2023-05-31 14:01:30 -0300

    aula-4

---
+++ 3-python-entendendo-oo/aula-4/conta.py
@@ -0,0 +1,20 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f'Construindo objeto ... {self}')
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'O titular {self.__titular} possui {self.__saldo} de saldo')
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
