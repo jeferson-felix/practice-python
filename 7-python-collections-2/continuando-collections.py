20d896eca57d5484e7aa2fbdcb8b8b5f2ad6a612
jeferson-felix <jeferson.felix2002@outlook.com>
2023-06-06 10:40:37 -0300

    aula-2

--- 7-python-collections-2/continuando-collections.py
+++ 7-python-collections-2/continuando-collections.py
@@ -6,3 +6,16 @@ print(usuarios_data_science.copy() | usuarios_machine_learning.copy())
 print(usuarios_data_science.copy() & usuarios_machine_learning.copy())
 print(usuarios_data_science.copy() - usuarios_machine_learning.copy())
 print(usuarios_data_science.copy() ^ usuarios_machine_learning.copy())
+
+print('')
+
+usuarios = {1, 5, 76, 34, 52, 13, 17}
+usuarios.add(765)
+usuarios = frozenset(usuarios)
+print(len(usuarios))
+
+print('')
+
+meu_texto = 'Em linguística a noção de texto é ampla e ainda aberta a uma definição mais precisa'
+
+print(set(meu_texto.split()))
