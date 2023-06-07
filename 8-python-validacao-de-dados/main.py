6792358072d6fb663b3ecaa46858ee0846987d14
jeferson-felix <jeferson.felix2002@outlook.com>
2023-06-07 13:36:42 -0300

    aula-4

--- 8-python-validacao-de-dados/main.py
+++ 8-python-validacao-de-dados/main.py
@@ -1,5 +1,6 @@
 from cpf_cnpj import Documento
 from telefones_br import TelefonesBR
+from datas import DatasBR
 
 # Exemplos
 exemplo_cpf = "14203643082"
@@ -10,8 +11,11 @@ exemplo_telefone = "5511956716541"
 documento_cpf = Documento.cria_documento(exemplo_cpf)
 documento_cnpj = Documento.cria_documento(exemplo_cnpj)
 telefone_objeto = TelefonesBR(exemplo_telefone)
+cadastro = DatasBR()
 
 # Print objetos
 print(documento_cpf)
 print(documento_cnpj)
 print(telefone_objeto)
+print(cadastro)
+print(cadastro.tempo_cadastro())
