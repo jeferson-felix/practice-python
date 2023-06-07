bf697e3e7134b55b67105e1e3e017773e5ed722f
jeferson-felix <jeferson.felix2002@outlook.com>
2023-06-07 14:23:14 -0300

    aula-5

---
+++ 8-python-validacao-de-dados/acesso_cep.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class BuscaEndereco:
+    def __init__(self, cep):
+        cep = str(cep)
+        if self.cep_e_valido(cep):
+            self.cep = cep
+        else:
+            raise ValueError("CEP Inválido")
+
+    def cep_e_valido(self, cep):
+        if len(cep) == 8:
+            return True
+        return False
+
+    def format_cep(self):
+        return f"{self.cep[:5]}-{self.cep[5:]}"
+
+    def acessa_via_cep(self):
+        url = f"https://viacep.com.br/ws/{self.cep}/json/"
+        r = requests.get(url)
+        dados = r.json()
+        return dados['bairro'], dados['localidade'], dados['uf']
+
+    def __str__(self):
+        return self.format_cep()
--- 8-python-validacao-de-dados/main.py
+++ 8-python-validacao-de-dados/main.py
@@ -1,21 +1,31 @@
 from cpf_cnpj import Documento
 from telefones_br import TelefonesBR
 from datas import DatasBR
+from acesso_cep import BuscaEndereco
 
 # Exemplos
 exemplo_cpf = "14203643082"
 exemplo_cnpj = "41217267000112"
 exemplo_telefone = "5511956716541"
+exemplo_cep = "04101300"
 
 # Criando objetos
 documento_cpf = Documento.cria_documento(exemplo_cpf)
 documento_cnpj = Documento.cria_documento(exemplo_cnpj)
 telefone_objeto = TelefonesBR(exemplo_telefone)
 cadastro = DatasBR()
+cep_objeto = BuscaEndereco(exemplo_cep)
+acesso = cep_objeto.acessa_via_cep()
+bairro, cidade, uf = cep_objeto.acessa_via_cep()
 
 # Print objetos
-print(documento_cpf)
-print(documento_cnpj)
-print(telefone_objeto)
-print(cadastro)
-print(cadastro.tempo_cadastro())
+print(f'CPF: {documento_cpf}')
+print(f'CNPJ: {documento_cnpj}\n')
+
+print(f'Telefone: {telefone_objeto}\n')
+
+print(f'Data do cadastro: {cadastro}')
+print(f'Tempo de cadastro: {cadastro.tempo_cadastro()}\n')
+
+print(f'CEP: {cep_objeto}')
+print(f'Bairro: {bairro}, Cidade: {cidade}, UF: {uf}')
